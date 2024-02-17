2fe6906b92aea12a1d50c269c3038596eab9f4b8
Viet Thai <vietthai.nguyen03@gmail.com>
2024-02-17 15:44:02 +0700

    [frontend] feat: Foundation for Login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { Container } from "react-bootstrap";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+        <div className="logoHolder">
+          <span className="appName">
+            <span className="logo1Half">Edu</span>
+            <span className="logo2Half">Link</span>
+          </span>
+        </div>
+        <Container className="content">
+          <Login/>
+        </Container>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,123 @@
+import { Row, Col, Form, Button } from "react-bootstrap";
+import "./Login.css";
+import { useState } from "react";
+
+const Login = (props) => {
+    const [controller, setController] = useState({
+        tutorLogin: false,
+        clientLogin: false
+    })
+
+    const openLogin = (event) => {
+        let target = event.target;
+        let name = target.name;
+        
+        setController(prevState => ({
+            ...prevState,
+            [name]: true
+        }));
+    }
+
+    const exitLogin = (event) => {
+        let target = event.target;
+        let name = target.name;
+        
+        setController(prevState => ({
+            ...prevState,
+            [name]: false
+        }));
+    }
+
+    const tutorSectionInitial = () => {
+        return (
+            <Row className="gy-3" >
+                <Col className="contentCenter" xxl={12} xl={12} lg={12} md={12} sm={12}>
+                    <span className="sectionName"><u>Edu Tutor</u></span>
+                </Col>
+                <Col className="contentCenter" xxl={12} xl={12} lg={12} md={12} sm={12}>
+                    <Button variant="success" className="button registerButton">Register As Tutor</Button>
+                </Col>
+                <Col className="contentCenter" xxl={12} xl={12} lg={12} md={12} sm={12}>
+                    <Button variant="primary" name="tutorLogin" onClick={(event) => {openLogin(event)}} className="button loginButton">Login As Tutor</Button>
+                </Col>
+            </Row>
+        );
+    }
+    
+    const tutorSectionLogin = () => {
+        return (
+            <Form style={{width: "100%", padding:"2%"}}>
+                <Form.Group className="mb-3" controlId="formTutorEmail">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter Email"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formTutorPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter Password"/>
+                </Form.Group>
+                <Form.Group className="mb-3 contentCenter">
+                    <Button variant="primary" className="button loginButton">Login As Tutor</Button>
+                </Form.Group>
+                <Form.Group className=" mb-3 contentCenter">
+                    <Button variant="danger" name="tutorLogin" onClick={(event) => {exitLogin(event)}} className="button">Return</Button>
+                </Form.Group>
+            </Form>
+        ) 
+    }
+    
+    const clientSectionInitial = () => {
+        return (
+            <Row className="gy-3" >
+                <Col className="contentCenter" xxl={12} xl={12} lg={12} md={12} sm={12}>
+                    <span className="sectionName"><u>Edu Client</u></span>
+                </Col>
+                <Col className="contentCenter" xxl={12} xl={12} lg={12} md={12} sm={12}>
+                    <Button variant="success" className="button registerButton">Register As Client</Button>
+                </Col>
+                <Col className="contentCenter" xxl={12} xl={12} lg={12} md={12} sm={12}>
+                    <Button variant="primary" name="clientLogin" onClick={(event) => {openLogin(event)}} className="button loginButton">Login As Client</Button>
+                </Col>
+            </Row>
+        );
+    }
+    
+    const clientSectionLogin = () => {
+        return (
+            <Form style={{width: "100%", padding:"2%"}}>
+                <Form.Group className="mb-3" controlId="formClientEmail">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter Email"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formClientPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter Password"/>
+                </Form.Group>
+                <Form.Group className="mb-3 contentCenter">
+                    <Button variant="primary" className="button loginButton">Login As Client</Button>
+                </Form.Group>
+                <Form.Group className=" mb-3 contentCenter">
+                    <Button variant="danger" name="clientLogin" onClick={(event) => {exitLogin(event)}} className="button">Return</Button>
+                </Form.Group>
+            </Form>
+        ) 
+    }
+
+    return (
+        <Row className="gx-4 gy-5">
+            <Col className="tutorCol" xxl={6} xl={6} lg={6} md={12} sm={12}>
+                <div className="tutorSection">
+                    {controller.tutorLogin === false && tutorSectionInitial()}
+                    {controller.tutorLogin === true && tutorSectionLogin()}
+                </div>
+            </Col>
+            <Col className="clientCol">
+                <div className="clientSection">
+                    {controller.clientLogin === false && clientSectionInitial()}
+                    {controller.clientLogin === true && clientSectionLogin()}
+                </div>
+            </Col>
+        </Row>
+    )
+}
+
+export default Login;
